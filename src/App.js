3fb499a0767ca8f8ac79e63ea91a7e8483034b15
Dilan <dio4@pitt.edu>
2020-12-20 19:27:11 -0500

    FilterExample()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { EasybaseProvider, useEasybase } from 'easybase-react';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { HashRouter, Switch, Route, Link } from 'react-router-dom';
 import ebconfig from './ebconfig';
 
@@ -11,13 +11,18 @@ function App() {
         <div style={{ display: "flex", justifyContent: "space-evenly", borderBottom: "1px grey solid" }}>
           <Link to="/"><h2>Home</h2></Link>
           <Link to="/starred"><h2>Starred</h2></Link>
+          <Link to="/over100"><h2>Over 100</h2></Link>
         </div>
+        <AuthButton />
         <Switch>
           <Route path="/" exact>
             <Home />
           </Route>
           <Route path="/starred" exact>
-            <p>Starred Products</p>
+            <Starred />
+          </Route>
+          <Route path="/over100" exact>
+            <FilterExample />
           </Route>
         </Switch>
       </HashRouter>
@@ -25,11 +30,69 @@ function App() {
   );
 }
 
+function AuthButton() {
+  const {
+    isUserSignedIn,
+    signIn,
+    signOut,
+    signUp,
+  } = useEasybase();
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [usernameValue, setUsernameValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const onAuthButtonClick = () => {
+    if (isUserSignedIn()) {
+      signOut();
+    } else {
+      setDialogOpen(true);
+    }
+  }
+
+  const onSignInClick = async () => {
+    const res = await signIn(usernameValue, passwordValue);
+    if (res.success) {
+      setDialogOpen(false);
+      setUsernameValue("");
+      setPasswordValue("");
+    }
+  }
+
+  const onSignUpClick = async () => {
+    const res = await signUp(usernameValue, passwordValue);
+    if (res.success) {
+      await signIn(usernameValue, passwordValue);
+      setDialogOpen("");
+      setUsernameValue("");
+      setPasswordValue("");
+    }
+  }
+
+  return (
+    <>
+      <button onClick={onAuthButtonClick} className="authButton">{isUserSignedIn() ? "Sign Out" : "Sign In"}</button>
+      <div className={dialogOpen ? "authDialog authDialogOpen" : "authDialog"} onClick={_ => setDialogOpen(false)}>
+        <div onClick={e => e.stopPropagation()}>
+          <input type="text" placeholder="Username" value={usernameValue} onChange={e => setUsernameValue(e.target.value)} />
+          <input type="password" placeholder="Password" value={passwordValue} onChange={e => setPasswordValue(e.target.value)} />
+          <div>
+            <button style={{ marginRight: 10, marginTop: 14, fontSize: 16 }} onClick={onSignInClick}>Sign In</button>
+            <button style={{ marginLeft: 10, marginTop: 14, fontSize: 16 }} onClick={onSignUpClick}>Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
 function Home() {
   const {
     configureFrame,
     sync,
-    Frame
+    Frame,
+    isUserSignedIn,
+    addRecord
   } = useEasybase();
 
   useEffect(() => {
@@ -37,37 +100,74 @@ function Home() {
     sync();
   }, [])
 
-  const cardRootStyle = {
-    width: 250,
-    height: 400,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
-    margin: 30,
-    padding: "0 10px"
-  }
-
-  const buttonStyle = {
-    paddingTop: 3,
-    fontSize: 16,
-    cursor: "pointer",
-    display: "inline-block",
-    marginBottom: 5,
-    borderRadius: 5,
-    backgroundColor: "#4CAF50",
-    color: "white",
-    fontWeight: 500,
+  const handleStarClick = (ele) => {
+    if (isUserSignedIn()) {
+      addRecord({
+        tableName: "USER STARS", newRecord: {
+          product_name: ele.product_name,
+          amazon_link: ele.amazon_link
+        }
+      })
+    }
   }
 
   return (
     <div style={{ display: "flex" }}>
-      {Frame().map(ele => 
-        <div style={cardRootStyle}>
-          <img src={ele.demo_image} style={{ objectFit: "contain", height: 270, width: "100%", marginBottom: 10 }} />
+      {Frame().map(ele =>
+        <div className="cardRoot">
+          <a href={ele.amazon_link}><img src={ele.demo_image} style={{ objectFit: "contain", height: 270, width: "100%", marginBottom: 10 }} /></a>
           <h4 style={{ textAlign: "center", borderTop: "1px grey solid", paddingTop: 15, margin: 0 }}>{ele.product_name}</h4>
           <p style={{ color: "green", textAlign: "end", fontWeight: 700 }}>${ele.price}</p>
-          <button style={buttonStyle} onClick={() => {}}>⭐ Save for later ⭐</button>
+          <button className="cardButton" onClick={_ => handleStarClick(ele)}>⭐ Save for later ⭐</button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+function Starred() {
+  const {
+    configureFrame,
+    sync,
+    Frame,
+  } = useEasybase();
+
+  useEffect(() => {
+    configureFrame({ limit: 10, offset: 0, tableName: "USER STARS" });
+    sync();
+  }, [])
+
+  return (
+    <div style={{ display: "flex" }}>
+      {Frame().map(ele =>
+        <div className="cardRoot" style={{ height: 50, alignItems: "center", justifyContent: "center" }}>
+          <a href={ele.amazon_link}>{ele.product_name}</a>
+        </div>
+      )}
+    </div>
+  )
+}
+
+function FilterExample() {
+  const {
+    Query
+  } = useEasybase()
+
+  const [arr, setArr] = useState([]);
+
+  useEffect(() => {
+    Query({ queryName: "price filter", tableName: "REACT DEMO" }).then(res => {
+      setArr(res);
+    });
+  }, [])
+
+  return (
+    <div style={{ display: "flex" }}>
+      {arr.map(ele =>
+        <div className="cardRoot">
+          <a href={ele.amazon_link}><img src={ele.demo_image} style={{ objectFit: "contain", height: 270, width: "100%", marginBottom: 10 }} /></a>
+          <h4 style={{ textAlign: "center", borderTop: "1px grey solid", paddingTop: 15, margin: 0 }}>{ele.product_name}</h4>
+          <p style={{ color: "green", textAlign: "end", fontWeight: 700 }}>${ele.price}</p>
         </div>
       )}
     </div>
